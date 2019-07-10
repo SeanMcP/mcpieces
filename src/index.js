4ca646ead985840c929918c8e3f16f872ed7b304
Sean McPherson <seanmcp25@gmail.com>
2019-07-09 22:00:08 -0400

    Add basic input

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import Input from 'form/Input/Input'
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export const Form = {
+  Input
 }
+
+import './index.scss'
