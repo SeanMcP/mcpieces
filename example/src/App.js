918dea755376a6b728108ab8988e8a3202a44c47
Sean McPherson <seanmcp25@gmail.com>
2019-07-11 21:15:20 -0400

    Remove example

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <ExampleComponent text="Modern React component module" />
         <Form.Input type="text" defaultValue="Hello world!" />
       </div>
     )
